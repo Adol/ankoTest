fefffe3884c743489d70840074d6cc1d10179162
Adol <adol.6977@gmail.com>
2018-04-29 21:38:54 +0800

    1.0

--- app/src/main/java/com/ankotest/adol/pickertest/ui/toolFun.kt
+++ app/src/main/java/com/ankotest/adol/pickertest/ui/toolFun.kt
@@ -2,6 +2,6 @@ package com.ankotest.adol.pickertest.ui
 
 import com.google.gson.Gson
 import org.json.JSONObject
-
-fun <T> T?.pln() = println(this)
+@Suppress("NOTHING_TO_INLINE")
+inline fun <T> T?.pln() = println(this)
 inline fun <reified T: Any> Gson.jsOB2class(json: JSONObject): T = this.fromJson(json.toString(), T::class.java)
